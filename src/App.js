4fe5ab6806f39d790e209c8ddcfd4ae663bcceb4
VuHoaBinh <hoabinh.vippro63@gmail.com>
2024-12-19 11:09:58 +0700

    update files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import Users from './user/pages/Users';
-import NewPlace from './places/pages/NewPlace';
-import UserPlaces from './places/pages/UserPlaces';
-import MainNavigation from './shared/components/Navigation/MainNavigation';
+import Users from "./user/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
+import UserPlaces from "./places/pages/UserPlaces";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
 const App = () => {
   return (
@@ -12,9 +12,9 @@ const App = () => {
       <MainNavigation />
       <main>
         <Routes>
-          <Route path="/" element={<Users />} exact/>
-          <Route path="/:userId/places" element={<UserPlaces />} exact/>
-          <Route path="/places/new" element={<NewPlace />} exact/>
+          <Route path="/" element={<Users />} exact />
+          <Route path="/:userId/places" element={<UserPlaces />} exact />
+          <Route path="/places/new" element={<NewPlace />} exact />
           {/* <Redirect to="/" /> */}
         </Routes>
       </main>
