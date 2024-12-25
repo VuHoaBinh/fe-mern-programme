866871f6107114f6991161331fdc4f245925a142
VuHoaBinh <hoabinh.vippro63@gmail.com>
2024-12-25 09:55:09 +0700

    use hook

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from 'react-router-dom';
 
-import Users from "./user/pages/Users";
-import NewPlace from "./places/pages/NewPlace";
-import UserPlaces from "./places/pages/UserPlaces";
-import MainNavigation from "./shared/components/Navigation/MainNavigation";
+import Users from './user/pages/Users';
+import NewPlace from './places/pages/NewPlace';
+import UserPlaces from './places/pages/UserPlaces';
+import UpdatePlace from './places/pages/UpdatePlace';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <Router>
       <MainNavigation />
       <main>
-        <Routes>
-          <Route path="/" element={<Users />} exact />
-          <Route path="/:userId/places" element={<UserPlaces />} exact />
-          <Route path="/places/new" element={<NewPlace />} exact />
-          {/* <Redirect to="/" /> */}
-        </Routes>
+        <Switch>
+          <Route path="/" exact>
+            <Users />
+          </Route>
+          <Route path="/:userId/places" exact>
+            <UserPlaces />
+          </Route>
+          <Route path="/places/new" exact>
+            <NewPlace />
+          </Route>
+          <Route path="/places/:placeId">
+            <UpdatePlace />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
       </main>
-    </BrowserRouter>
+    </Router>
   );
 };
 
--- src/places/pages/NewPlaces.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function NewPlaces() {
-  return <h1>hello New places page</h1>;
-}
-
-export default NewPlaces;
